0729b534f5ab21702a9965a092d5d3fd457dbd1c
Serhii Dotsenko <serdotsenko@gmail.com>
2024-02-03 21:02:51 +0000

    JavaScript logic was finished

--- script.js
+++ script.js
@@ -1,81 +1,25 @@
-let a = ''; // first number
-let b = ''; // second number
-let sign = ''; // знак операции
-let finish  = false;
+const arr = [];
+const display = document.querySelector('.calc-screen p');
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
-const action = ['-', '+', 'X', '/'];
+document.querySelector('.ac').onclick = () => {
+    display.textContent = '0';
+    arr.length = 0;
+    console.log(arr);
+};
 
-const out = document.querySelector('.calc-screen p');
+document.querySelector('.buttons').onclick = event => {
+    const concatDigits = () => arr.length ? arr.join('') : '0';
+    const symbol = event.target.textContent.charAt(0) === 'X' ? '*' : event.target.textContent.charAt(0);
 
-function clearAll () {
-    a = ''; // first number and result
-    b = ''; // second number
-    sign = ''; // знак
-    finish = false;
-    out.textContent = '0';
-}
-
-document.querySelector('.ac').onclick = clearAll;
-
-document.querySelector('.buttons').onclick = (event) => {
-    if(!event.target.classList.contains('btn')) return;
-    if(event.target.classList.contains('ac')) return;
-
-    out.textContent = '';
-    const key = event.target.textContent;
-
-    if (digit.includes(key)) {
-        if (b ==='' && sign === '') {
-            a += key;
-
-            out.textContent = a;
-        }
-        else if (a!=='' && b!=='' && finish) {
-            b = key;
-            finish = false;
-            out.textContent = b;
-        }
-        else {
-            b += key;
-            out.textContent = b;
-        }
-        console.table(a, b , sign);
-        return;
+    if (/[0-9.]/.test(symbol) || /[+\-*\/%]/.test(symbol)) {
+        arr.push(symbol)
+        display.textContent = concatDigits();
+    } else if (symbol === '←') {
+        arr.pop();
+        display.textContent = concatDigits();
+    } else if (symbol === '=') {
+        display.textContent += `=${eval(concatDigits())}`;
+        arr.length = 0;
     }
-
-    if (action.includes(key)) {
-        sign = key;
-        out.textContent = sign;
-        console.table(a, b , sign);
-        return;
-    }
-
-    if (key === '=') {
-        if (b ==='') b = a;
-        switch (sign) {
-            case "+":
-                a = (+a) + (+b);
-                break;
-            case "-":
-                a = a - b;
-                break;
-            case "X":
-                a = a * b;
-                break;
-            case "/":
-                if (b === '0') {
-                    out.textContent = 'Ошибка';
-                    a = '';
-                    b = '';
-                    sign = '';
-                    return;
-                }
-                a = a / b;
-                break;
-        }
-        finish = true;
-        out.textContent = a;
-        console.table(a, b , sign);
-    }
+    console.log(arr);
 }
