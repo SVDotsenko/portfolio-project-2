abd663fc51cac0b5752af6696ce8d6f23d404d62
Serhii Dotsenko <serdotsenko@gmail.com>
2024-02-05 10:40:39 +0000

    refactoring

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
+// добавить медиа в цсс, убрать верхний отступ на мобильных устройсвах
 const arr = [];
 const display = document.querySelector('.calc-screen p');
 
 document.querySelector('.ac').onclick = () => {
     display.textContent = '0';
     arr.length = 0;
-    console.log(arr);
+};
+
+const isArithmeticOperator = symbol => /[+\-*\/%]/.test(symbol);
+const isDot = symbol => /\./.test(symbol);
+
+const dotValidator = () => {
+    for (let i = arr.length - 1; i >= 0; i--) {
+        if (isDot(arr[i])) {
+            return true;
+        }
+        if (isArithmeticOperator(arr[i])) {
+            return false;
+        }
+    }
+    return false;
 };
 
 document.querySelector('.buttons').onclick = event => {
-    const concatDigits = () => arr.length ? arr.join('') : '0';
-    const symbol = event.target.textContent.charAt(0) === 'X' ? '*' : event.target.textContent.charAt(0);
+    const isZero = symbol => symbol == 0;
+    const isDigit = symbol => /[1-9]/.test(symbol);
+    const zeroValidator = () => arr[0] == 0 && arr.length === 1;
+    const concatSymbols = () => arr.length ? arr.join('') : '0';
+    const currentSymbol = event.target.textContent.charAt(0) === 'X' ? '*' : event.target.textContent.charAt(0);
+    const lastSymbol = arr.at(-1);
 
-    if (/[0-9.]/.test(symbol) || /[+\-*\/%]/.test(symbol)) {
-        arr.push(symbol)
-        display.textContent = concatDigits();
-    } else if (symbol === '←') {
+    if (isZero(currentSymbol)) {
+        if (zeroValidator()) {
+            alert('You can not use two zero in succession');
+        } else {
+            arr.push(currentSymbol);
+            display.textContent = concatSymbols();
+        }
+    } else if (isDot(currentSymbol)) {
+        if (dotValidator()) {
+            alert('You can not use dot here');
+        } else {
+            arr.push(currentSymbol);
+            display.textContent = concatSymbols();
+        }
+    } else if (isArithmeticOperator(currentSymbol)) {
+        isArithmeticOperator(lastSymbol) ? arr[arr.length - 1] = currentSymbol : arr.push(currentSymbol);
+        display.textContent = concatSymbols();
+    } else if (isDigit(currentSymbol)) {
+        arr.push(currentSymbol);
+        display.textContent = concatSymbols();
+    } else if (currentSymbol === '←') {
         arr.pop();
-        display.textContent = concatDigits();
-    } else if (symbol === '=') {
-        display.textContent += `=${eval(concatDigits())}`;
-        arr.length = 0;
+        display.textContent = concatSymbols();
+    } else if (currentSymbol === '=') {
+        if (lastSymbol !== '.' && (isDigit(lastSymbol) || isZero(lastSymbol))) {
+            try {
+                display.textContent += `=${eval(concatSymbols())}`;
+                arr.length = 0;
+            } catch (e) {
+                alert('This expression can not be calculated. Change it and try again.');
+            }
+        } else {
+            alert('The last symbol must be a number');
+        }
     }
     console.log(arr);
 }
