ddb5bc7de4828ad12d8e0fdafefd54c3f201a084
Serhii Dotsenko <serdotsenko@gmail.com>
2024-02-08 08:06:04 +0000

    update: file paths, comments in JavaScript added

--- README.md
+++ README.md
@@ -1,17 +1,17 @@
 # Calculator
 
-This website is a simple implementation of the Calculator with basic arithmetic operations: +, -, *, /, %.
+This website is a simple implementation of the Calculator with basic arithmetic operations: +, -, \*, /, %.
 
 The main purpose of this site is demonstrate coding skills in JavaScript, HTML and CSS.
 
 This website is implemented only for education purpose.
 
-![Mockup](images/desktop.png)
+![Mockup](assets/images/desktop.png)
 
 ## Features
 
-The calculator can do basic arithmetic operation,  +, -, *, /, %. Each operator works according mathematics rules except %.
-The operator % conduct remainder of the division. 
+The calculator can do basic arithmetic operation, +, -, \*, /, %. Each operator works according mathematics rules except %.
+The operator % conduct remainder of the division.
 
 For example:
 
@@ -21,42 +21,44 @@ For example:
 
 5 % 2 = 1
 
-Calculations are carried out taking into account the priority of operators which is accepted in mathematics. 
+Calculations are carried out taking into account the priority of operators which is accepted in mathematics.
 
 For example:
 
-1 + 2 * 3 = 7
+1 + 2 \* 3 = 7
 
 ## Testing
 
-* I tested that this page works in different browsers: Chrome, Firefox, Safari.
-* I confirmed that this project is responsive, looks good and functions оп ап standard screen sizes using the
-devtools device toolbar.
+-   I tested that this page works in different browsers: Chrome, Firefox, Safari.
+-   I confirmed that this project is responsive, looks good and functions оп ап standard screen sizes using the
+    devtools device toolbar.
 
 ### Validator Testing
 
-- HTML
-    - No errors were returned when passing through the official W3C validator.
-  
-![W3C validator](images/html.png)
-- CSS
-    - No errors were found when passing through the official (Jigsaw) validator.
+-   HTML
+    -   No errors were returned when passing through the official W3C validator.
 
-![W3C validator](images/css.png)
+![W3C validator](assets/images/html.png)
 
-- JSHint
-    - No errors were found when passing through the JSHint validator according with settings in .jshintrc file.
+-   CSS
+    -   No errors were found when passing through the official (Jigsaw) validator.
 
-- Accessibility  
-  - I confirm that the colors and fonts chosen are easy to read and accessible by running it through lighthouse in devtools. 
+![W3C validator](assets/images/css.png)
 
-![lighthouse](images/lighthouse.png)
+-   JSHint
+
+    -   No errors were found when passing through the JSHint validator according with settings in .jshintrc file.
+
+-   Accessibility
+    -   I confirm that the colors and fonts chosen are easy to read and accessible by running it through lighthouse in devtools.
+
+![lighthouse](assets/images/lighthouse.png)
 
 ## Deployment
 
-- The site was deployed to GitHub pages. The steps to deploy are as follows:
-    - In the GitHub repository, navigate to the Settings tab
-    - From the source section drop-down menu, select the main branch
-    - Once the main branch has been selected, the page will be automatically refreshed with a detailed ribbon display to indicate the successful deployment.
+-   The site was deployed to GitHub pages. The steps to deploy are as follows:
+    -   In the GitHub repository, navigate to the Settings tab
+    -   From the source section drop-down menu, select the main branch
+    -   Once the main branch has been selected, the page will be automatically refreshed with a detailed ribbon display to indicate the successful deployment.
 
 The live link can be found [here](https://svdotsenko.github.io/portfolio-project-2)
--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,24 @@ document.querySelector('.ac').onclick = () => {
     arr.length = 0;
 };
 
+/**
+ * Checks if a symbol is an arithmetic operator.
+ * @param {string} symbol - The symbol to be checked.
+ * @returns {boolean} - True if the symbol is an arithmetic operator, false otherwise.
+ */
 const isArithmeticOperator = symbol => /[+\-*\/%]/.test(symbol);
+
+/**
+ * Checks if a symbol is a dot.
+ * @param {string} symbol - The symbol to check.
+ * @returns {boolean} - True if the symbol is a dot, false otherwise.
+ */
 const isDot = symbol => /\./.test(symbol);
 
+/**
+ * Checks if the given array contains a dot.
+ * @returns {boolean} True if the array contains a dot, false otherwise.
+ */
 const dotValidator = () => {
     for (let i = arr.length - 1; i >= 0; i--) {
         if (isDot(arr[i])) {
@@ -22,37 +37,58 @@ const dotValidator = () => {
 };
 
 document.querySelector('.buttons').onclick = event => {
+    /**
+     * Checks if a symbol is zero.
+     * @param {string} symbol - The symbol to check.
+     * @returns {boolean} Returns true if the symbol is zero, otherwise returns false.
+     */
     const isZero = symbol => symbol === '0';
+
+    /**
+     * Checks if a symbol is a digit in range from 1 to 9.
+     * @param {string} symbol - The symbol to be checked.
+     * @returns {boolean} - Returns true if the symbol is a digit, otherwise false.
+     */
     const isDigit = symbol => /[1-9]/.test(symbol);
+
+    /**
+     * Checks if the first element of the array is '0' and the array has a length of 1.
+     * @returns {boolean} True if the first element is '0' and the array has a length of 1, otherwise false.
+     */
     const zeroValidator = () => arr[0] === '0' && arr.length === 1;
+
+    /**
+     * Concatenates the elements of an array into a single string using a specified separator.
+     * @returns {string} The concatenated string or '0' if the array is empty.
+     */
     const concatSymbols = () => arr.length ? arr.join('') : '0';
     const currentSymbol = event.target.textContent.charAt(0) === 'X' ? '*' : event.target.textContent.charAt(0);
     const lastSymbol = arr.at(-1);
 
-    if (isZero(currentSymbol)) {
+    if (isZero(currentSymbol)) {//if the user pressed the zero button
         if (zeroValidator()) {
             alert('You can not use two zero in succession');
         } else {
             arr.push(currentSymbol);
             display.textContent = concatSymbols();
         }
-    } else if (isDot(currentSymbol)) {
+    } else if (isDot(currentSymbol)) {//if the user pressed the dot button
         if (dotValidator()) {
             alert('You can not use dot here');
         } else {
             arr.push(currentSymbol);
             display.textContent = concatSymbols();
         }
-    } else if (isArithmeticOperator(currentSymbol)) {
+    } else if (isArithmeticOperator(currentSymbol)) {//if the user pressed an arithmetic operator
         isArithmeticOperator(lastSymbol) ? arr[arr.length - 1] = currentSymbol : arr.push(currentSymbol);
         display.textContent = concatSymbols();
-    } else if (isDigit(currentSymbol)) {
+    } else if (isDigit(currentSymbol)) {//if the user pressed a digit from 1 to 9
         arr.push(currentSymbol);
         display.textContent = concatSymbols();
-    } else if (currentSymbol === '←') {
+    } else if (currentSymbol === '←') {//if the user pressed the backspace button
         arr.pop();
         display.textContent = concatSymbols();
-    } else if (currentSymbol === '=') {
+    } else if (currentSymbol === '=') {//if the user pressed the equal button
         if (lastSymbol !== '.' && (isDigit(lastSymbol) || isZero(lastSymbol))) {
             try {
                 display.textContent += `=${eval(concatSymbols())}`;
@@ -64,5 +100,4 @@ document.querySelector('.buttons').onclick = event => {
             alert('The last symbol must be a number');
         }
     }
-    console.log(arr);
 };
